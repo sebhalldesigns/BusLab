ace4343b7e339e4cbeffc3f30bbffb54e9883f94
sebhall <sebhalldesigns@gmail.com>
2025-10-29 15:51:12 +0000

    added panel selection

--- Panels/BlankPanel.axaml.cs
+++ Panels/BlankPanel.axaml.cs
@@ -1,14 +1,71 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System;
 
 namespace BusLab;
 
+public enum PanelType
+{
+    Blank,
+    NetworkMessages,
+    NetworkTrace,
+    SignalPlot,
+    DatabaseFile
+}
+
 public partial class BlankPanel: UserControl
 {
-
+    Dock.Model.Avalonia.Controls.Document parent;
+     
     public BlankPanel(Dock.Model.Avalonia.Controls.Document parent)
     {
+        this.parent = parent;
+
         InitializeComponent();
+    }
+
+    private void NetworkMessagesPressed(object? sender, RoutedEventArgs e)
+    {
+        LoadPanel(PanelType.NetworkMessages);
+    }
+
+    private void NetworkTracePressed(object? sender, RoutedEventArgs e)
+    {
+        LoadPanel(PanelType.NetworkTrace);
+    }
+
+    private void SignalPlotPressed(object? sender, RoutedEventArgs e)
+    {
+        LoadPanel(PanelType.SignalPlot);
+    }
+
+    private void DatabaseFilePressed(object? sender, RoutedEventArgs e)
+    {
+        LoadPanel(PanelType.DatabaseFile);
+    }
+    
+    public void LoadPanel(PanelType panel)
+    {
+        switch (panel)
+        {
+            case PanelType.NetworkMessages:
+                PanelContent.Content = new NetworkMessagesPanel();
+                SelectPanelContent.IsVisible = false;
+                break;
+
+            case PanelType.SignalPlot:
+                PanelContent.Content = new SignalPlotPanel();
+                SelectPanelContent.IsVisible = false;
+                break;
+
+            default:
+                break;
+        }
+
+        parent.Title = panel.ToString();
+
+        Console.WriteLine("Loading panel: " + panel.ToString());
     }
 }
