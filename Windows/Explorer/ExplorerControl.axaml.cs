ef53cf6d72a3c18b465586576e403a0b92670ad6
sebhall <sebhalldesigns@gmail.com>
2025-11-02 17:21:50 +0000

    working on explorer

--- Windows/Explorer/ExplorerControl.axaml.cs
+++ Windows/Explorer/ExplorerControl.axaml.cs
@@ -1,14 +1,86 @@
 using Avalonia.Controls;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using Avalonia;
+using Avalonia.Media;
+using Avalonia.Interactivity;
+using Avalonia.VisualTree;
+using Material.Icons.Avalonia;
+using Material.Icons;
 
 namespace BusLab;
 
+public class ExplorerEntry
+{
+    public string Name { get; set; } = "New Entry";
+    public string Path { get; set; } = "C:\\Path\\To\\Entry";
+    public MaterialIconKind? Icon { get; set; } = null;
+    public bool IconVisible { get; set; } = false;
+    public Thickness LabelMargin { get; set; } = new Thickness(0, 0, 0, 0);
+    public ObservableCollection<ExplorerEntry> Children { get; set; } = new ObservableCollection<ExplorerEntry>();
+}
+
 public partial class ExplorerControl: UserControl
 {
+    public ObservableCollection<ExplorerEntry> Entries { get; set; } = new ObservableCollection<ExplorerEntry>();
+
     public ExplorerControl()
     {
         InitializeComponent();
 
+        // Sample data
+        Entries.Add(new ExplorerEntry
+        {
+            Name = "Documents",
+            Path = "C:\\Users\\User\\Documents",
+            Children = new ObservableCollection<ExplorerEntry>
+            {
+                new ExplorerEntry {
+                    Name = "MyNet.net", 
+                    Path = "C:\\Users\\User\\Documents\\Resume.dbc",
+                    LabelMargin = new Thickness(20, 0, 0, 0),
+                    Icon = MaterialIconKind.Lan,
+                    IconVisible = true
+                },
+                new ExplorerEntry {
+                    Name = "ExampleFilter.filter", 
+                    Path = "C:\\Users\\User\\Documents\\Resume.dbc",
+                    LabelMargin = new Thickness(20, 0, 0, 0),
+                    Icon = MaterialIconKind.FilterOutline,
+                    IconVisible = true
+                },
+                new ExplorerEntry {
+                    Name = "Network.dbc", 
+                    Path = "C:\\Users\\User\\Documents\\Resume.dbc",
+                    LabelMargin = new Thickness(20, 0, 0, 0),
+                    Icon = MaterialIconKind.DatabaseOutline,
+                    IconVisible = true
+                },
+                new ExplorerEntry {
+                    Name = "ExamplePlot.plot", 
+                    Path = "C:\\Users\\User\\Documents\\Resume.dbc",
+                    LabelMargin = new Thickness(20, 0, 0, 0),
+                    Icon = MaterialIconKind.ChartLine,
+                    IconVisible = true
+                },
+                new ExplorerEntry {
+                    Name = "ExampleCanvas.canvas", 
+                    Path = "C:\\Users\\User\\Documents\\Resume.dbc",
+                    LabelMargin = new Thickness(20, 0, 0, 0),
+                    Icon = MaterialIconKind.Numeric,
+                    IconVisible = true
+                },
+            }
+        });
+
+        this.DataContext = this;
+
+    }
+
+    public void NewPressed(object? sender, RoutedEventArgs e)
+    {
+        NewWindow newWindow = new NewWindow();
+        newWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+        newWindow.ShowDialog(this.GetVisualRoot() as Window);
     }
 }
--- Windows/NewWindow.axaml.cs
+++ Windows/NewWindow.axaml.cs
@@ -1,40 +1,56 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media;
 using System;
-
+using Avalonia;
 
 namespace BusLab;
 
 public partial class NewWindow : Window
 {
+    private Button? selectedButton = null;
+    private IBrush unselectedBrush = Brushes.Gray;
+    private IBrush selectedBrush = Brushes.LightBlue;
+
     public NewWindow()
     {
         InitializeComponent();
-        TabContent.Content = new NewNetControl();
-    }
 
-    private void TabSelectionChanged(object? sender, SelectionChangedEventArgs e)
-    {
-        if (TabListBox == null || TabContent == null)
+        unselectedBrush = Brushes.Transparent;
+
+        if (Application.Current!.TryGetResource("ThemeControlMidBrush", App.Current!.ActualThemeVariant, out var brush))
         {
-            return;
+            selectedBrush = brush as IBrush ?? Brushes.Gray;
         }
 
-        switch (TabListBox.SelectedIndex)
-        {
-            case 0:
-                TabContent.Content = new NewNetControl();
-                break;
-            case 1:
-                TabContent.Content = new NewCanDatabaseControl();
-                break;
-            default:
-                break;
-        }
+        selectedButton = NewNetButton;
+        UpdateHighlights();
     }
 
     public void ClosePressed(object? sender, RoutedEventArgs e)
     {
         Close();
+    }
+
+    public void NewButtonPressed(object? sender, RoutedEventArgs e)
+    {
+        if (sender is not Button button)
+            return;
+
+        selectedButton = button;
+        UpdateHighlights();
+    }
+
+    private void UpdateHighlights()
+    {
+        if (selectedButton == null)
+            return;
+
+        NewNetButton.Background = unselectedBrush;
+        NewFilterButton.Background = unselectedBrush;
+        NewCanDatabaseButton.Background = unselectedBrush;
+
+        selectedButton.Background = selectedBrush;
+
     }
 }
