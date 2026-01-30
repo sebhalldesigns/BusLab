c2c28bf1f3068d9180dd1be6b2fec0f2d868a229
sebhall <sebhalldesigns@gmail.com>
2026-01-30 11:02:55 +0000

    added content area overlay

--- Workbench.cs
+++ Workbench.cs
@@ -17,11 +17,27 @@ namespace BusLab;
 
 public class Workbench: UserControl
 {
+    public List<TabGroup> TabGroups = new List<TabGroup>(); 
+
     public Workbench()
     {
         Console.WriteLine("Hello from Workbench");
 
-        Content = new TabGroup();
+        Content = new TabGroup(this);
+        TabGroups.Add((TabGroup)Content);
+    }
+
+    public void TabDragged(Tab tab, PointerEventArgs e)
+    {
+        Console.WriteLine("Tab dragged: " + tab.TextBlock.Text + " at " + e.GetPosition(this));
+        
+        foreach (TabGroup group in TabGroups)
+        {
+            if (group.TabDragged(tab, e))
+            {
+                return;
+            }
+        }
     }
 }
 
@@ -29,9 +45,16 @@ public class TabGroup: UserControl
 {
 
     public List<Tab> Tabs = new List<Tab>();
+    public ContentControl ContentControl;
 
-    public TabGroup()
+    private Workbench workbench;
+    private Grid contentGrid;
+    private Panel overlayPanel;
+
+    public TabGroup(Workbench workbench)
     {
+        this.workbench = workbench;
+
         Grid grid = new Grid();
         grid.RowDefinitions.Add(new RowDefinition(25.0f, GridUnitType.Pixel));
         grid.RowDefinitions.Add(new RowDefinition(GridLength.Star));
@@ -40,22 +63,36 @@ public class TabGroup: UserControl
         Grid.SetRow(scrollViewer, 0);
         scrollViewer.Background = new Avalonia.Media.SolidColorBrush(0x30808080);
 
-        ContentControl contentControl = new ContentControl();
-        contentControl.Content = new TextBlock { Text = "Content goes here." };
-        Grid.SetRow(contentControl, 1);
+        contentGrid = new Grid();
+        contentGrid.RowDefinitions.Add(new RowDefinition(GridLength.Star));
+        contentGrid.RowDefinitions.Add(new RowDefinition(GridLength.Star));
+        contentGrid.ColumnDefinitions.Add(new ColumnDefinition(GridLength.Star));
+        contentGrid.ColumnDefinitions.Add(new ColumnDefinition(GridLength.Star));
 
-        grid.Children.Add(contentControl);
+        Grid.SetRow(contentGrid, 1);
+    
+        ContentControl = new ContentControl();
+        ContentControl.Content = new TextBlock { Text = "Content goes here." };
+        Grid.SetRow(ContentControl, 0);
+        Grid.SetRowSpan(ContentControl, 2);
+        Grid.SetColumn(ContentControl, 0);
+        Grid.SetColumnSpan(ContentControl, 2);
+        contentGrid.Children.Add(ContentControl);
+
+        grid.Children.Add(contentGrid);
         grid.Children.Add(scrollViewer);
-        
-
+    
         Content = grid;
+
+        overlayPanel = new Panel();
+        overlayPanel.Background = new SolidColorBrush(0x804787d1);
 
         StackPanel tabPanel = new StackPanel { Orientation = Avalonia.Layout.Orientation.Horizontal };
         scrollViewer.Content = tabPanel;
 
         for (int i = 0; i < 5; i++)
         {
-            Tab tab = new Tab();
+            Tab tab = new Tab(workbench);
             tabPanel.Children.Add(tab);
             Tabs.Add(tab);
             tab.TextBlock.Text = "Tab " + (i + 1);
@@ -76,10 +113,83 @@ public class TabGroup: UserControl
             }
         }
     }
+
+    public bool TabDragged(Tab tab, PointerEventArgs e)
+    {
+        Point position = e.GetPosition(this);
+        
+        if (!this.Bounds.Contains(position))
+        {
+            if (overlayPanel.Parent != null)
+            {
+                contentGrid.Children.Remove(overlayPanel);
+            }   
+
+            return false;
+        }
+
+        if (position.Y < 25)
+        {
+            /* handle tab bar area */
+            if (overlayPanel.Parent != null)
+            {
+                contentGrid.Children.Remove(overlayPanel);
+            }   
+        }
+        else
+        {
+            /* handle content area */
+            
+            double xProportion = position.X / this.Bounds.Width;
+            double yProportion = (position.Y - 25) / (this.Bounds.Height - 25);
+
+            Grid.SetRow(overlayPanel, 0);
+            Grid.SetRowSpan(overlayPanel, 2);
+
+            if (xProportion < 0.2)
+            {
+                Grid.SetColumn(overlayPanel, 0);
+                Grid.SetColumnSpan(overlayPanel, 1);
+            }
+            else if (xProportion > 0.8)
+            {
+                Grid.SetColumn(overlayPanel, 1);
+                Grid.SetColumnSpan(overlayPanel, 1);
+            }
+            else
+            {
+                Grid.SetColumn(overlayPanel, 0);
+                Grid.SetColumnSpan(overlayPanel, 2);
+
+                if (yProportion < 0.2)
+                {
+                    Grid.SetRow(overlayPanel, 0);
+                    Grid.SetRowSpan(overlayPanel, 1);
+                }
+                else if (yProportion > 0.8)
+                {
+                    Grid.SetRow(overlayPanel, 1);
+                    Grid.SetRowSpan(overlayPanel, 1);
+                }
+            }
+
+           
+            if (overlayPanel.Parent == null)
+            {
+                contentGrid.Children.Add(overlayPanel);
+            }   
+            
+        }
+
+
+        return true;
+    }
 }
 
 public class Tab: UserControl
 {
+
+    private Workbench workbench;
 
     public bool IsSelected { get => GetIsSelected(); set => SetIsSelected(value); }
     private bool isSelected = false;
@@ -96,8 +206,10 @@ public class Tab: UserControl
     private Control? dragAdorner;
     private AdornerLayer? adornerLayer;
 
-    public Tab()
+    public Tab(Workbench workbench)
     {
+        this.workbench = workbench;
+
         /* create a new button on top of label */
         
         Button = new Button();
@@ -229,6 +341,8 @@ public class Tab: UserControl
         // Position the adorner (offset by where user grabbed it)
         Canvas.SetLeft(dragAdorner, adornerPosition.X - dragStartPoint.X);
         Canvas.SetTop(dragAdorner, adornerPosition.Y - dragStartPoint.Y);
+
+        workbench.TabDragged(this, e);
         
         Console.WriteLine($"Dragging tab by {delta}");
     }
