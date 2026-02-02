79f4f8d629b901d3e7dc0942034e251c38bbee40
seb hall <sebhalldesigns@gmail.com>
2026-02-02 21:29:54 +0000

    added template explorer panel

---
+++ Workbench/Explorer/Explorer.xaml.cs
@@ -0,0 +1,30 @@
+using Avalonia;
+using Avalonia.Styling;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text;
+
+
+namespace BusLab.Explorer;
+
+public partial class Explorer : UserControl
+{   
+
+    public Explorer()
+    {
+        InitializeComponent();        
+
+      
+    }
+
+
+
+}
+
+
+
